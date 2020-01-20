1dfbf465f20d4dab81b03fb36dc32503ecaa6deb
prokim <sei0044@gmail.com>
2020-01-20 09:41:25 +0900

    f

--- bk/v01/resource/js/main.js
+++ bk/v01/resource/js/main.js
@@ -1,36 +1,5 @@
 import Putity from "./Putity/Putity";
 
-class FF extends Putity {
-  constructor(h) {
-    super(h);
-  }
-  setState() {
-    return (self.state = {
-      test2: "test2"
-    });
-  }
-  sender2() {
-    console.log("test");
-    this.changeState("test2", "child change message");
-  }
-  run() {
-    window.ff = this;
-  }
-  render() {
-    return `<p>
-                sub module -------- ${this.state.test2}
-            </p>
-            <button onClick="window.ff.sender2()">click</button>
-                `;
-  }
-}
-
-const ff = new FF({
-  props: {
-    name: "new name"
-  }
-});
-ff.run();
 /**
  * extends PutiFlame
  */
@@ -52,7 +21,7 @@ class F extends Putity {
     this.changeState("goukei", temp);
   }
   run() {
-    window.f = this;
+     window.f = this;    
   }
 
   render() {
@@ -63,7 +32,6 @@ class F extends Putity {
         <br>this is ${self.state.test}
         <br>goukei is ${self.state.goukei}
         <span id="app2"></span>
-        ${ff.render()}
         </p>
         <button onClick="window.f.sender()">test</button>  
         `;
@@ -77,4 +45,43 @@ const f = new F({
   targetDom: "app"
 });
 f.run();
-//ff.setjoin(f);    
+
+
+function sender2(){
+    console.log("fda");
+}
+class FF extends Putity {
+  constructor(h) {
+    super(h);
+  }
+  setState() {
+    return (self.state = {
+      test: "test2 message",
+      goukei: 1
+    });
+  }
+  sender2() {
+    //console.log("test");
+    this.changeState("test", "child change message");
+    
+  }
+  run() {
+    self.ff = this;
+  }
+  render() {
+    return `<p>
+              sub module -------- ${this.state.test}
+          </p>
+          <button onClick="self.ff.sender2()">click</button>
+              `;
+  }
+}
+
+const ff = new FF({
+  props: {
+    name: "new name"
+  },
+  targetDom: "app2"
+});
+ff.run();
+f.setjoin(ff);
